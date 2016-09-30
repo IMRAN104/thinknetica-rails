5680741394623b51d709071c3b487d86738bd6df
Dima Rogov <navydimka@gmail.com>
2016-09-30 22:08:09 +0300

    auto set a position for passenger car within train during creation

--- app/models/car/passenger_car.rb
+++ app/models/car/passenger_car.rb
@@ -1,10 +1,13 @@
 class PassengerCar < ApplicationRecord
   belongs_to :train
-  validates :number, presence: true
-  before_save :default_values
 
-  scope :upper_count, -> { sum(:upper_seats) } 
-  scope :lower_count, -> { sum(:lower_seats) } 
+  validates :number, presence: true
+  validate :uniqe_position
+
+  before_save :set_position
+
+  scope :upper_count, -> { sum(:upper_seats) }
+  scope :lower_count, -> { sum(:lower_seats) }
 
   def self.list_of_types
     types = {}
@@ -16,8 +19,15 @@ class PassengerCar < ApplicationRecord
 
   private
 
-  def default_values 
-    self.upper_seats ||= 0
-    self.lower_seats ||= 0
+  def uniqe_position
+    if self.train.passenger_cars.where(position: self.position).empty?
+      errors.add(:passenger_cars, "Not uniqe position of passenger car within train!")
+    end
+  end
+
+  def set_position
+    positions = self.train.passenger_cars.map { |c| c.position.to_i }
+    gap = (1...positions.max).detect { |p| position.find_index(p).nil? }
+    self.position ||= gap.nil? ? positions.max + 1 : gap
   end
 end
