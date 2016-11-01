314a1ef216ec1feeba572794814ada4bd7d0c924
Dima Rogov <navydimka@gmail.com>
2016-11-01 03:06:39 +0300

    added new field for storing and viewing arrival and departure time for station within route

--- app/controllers/railway_stations_controller.rb
+++ app/controllers/railway_stations_controller.rb
@@ -1,5 +1,7 @@
 class RailwayStationsController < ApplicationController
-  before_action :set_railway_station, only: [:show, :edit, :update, :destroy, :update_position]
+  before_action :set_railway_station, only: [:show, :edit, :update, :destroy, 
+                                             :update_position, :update_time]
+  before_action :set_route, only: [:update_position, :update_time]
 
   def index
     @railway_stations = RailwayStation.all
@@ -39,8 +41,14 @@ class RailwayStationsController < ApplicationController
   end
 
   def update_position
-    @route = Route.find(params[:route_id])
-    @railway_station.update_position(@route, params[:position])
+    @railway_station.update_position(@route, params[:position]) 
+    redirect_to @route
+  end
+
+  def update_time
+    arrival = params[:arrival].try(:to_time)
+    departure = params[:departure].try(:to_time)
+    @railway_station.update_time(@route, arrival, departure)
     redirect_to @route
   end
 
@@ -48,6 +56,10 @@ class RailwayStationsController < ApplicationController
   
   def set_railway_station
     @railway_station = RailwayStation.find(params[:id])
+  end
+
+  def set_route
+    @route = Route.find(params[:route_id])
   end
 
   def railway_station_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161002193856) do
+ActiveRecord::Schema.define(version: 20161030230431) do
 
   create_table "passenger_cars", force: :cascade do |t|
     t.string   "number"
@@ -42,9 +42,11 @@ ActiveRecord::Schema.define(version: 20161002193856) do
   create_table "schedules", force: :cascade do |t|
     t.integer  "route_id"
     t.integer  "railway_station_id"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
     t.integer  "order",              default: 0
+    t.time     "arrival",            default: '2000-01-01 12:00:00'
+    t.time     "departure",          default: '2000-01-01 21:00:00'
   end
 
   create_table "tickets", force: :cascade do |t|
